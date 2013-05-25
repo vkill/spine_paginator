67b4f6cb7bbc96a442ac88a12dad4fa28016b216
vkill <vkill.net@gmail.com>
2013-05-25 18:51:25 +0800

    fix rails  couldn't find file 'spine.paginator' bug

--- lib/spine_paginator/railtie.rb
+++ lib/spine_paginator/railtie.rb
@@ -2,9 +2,10 @@
 module SpinePaginator
   class Railtie < ::Rails::Railtie
   
-    initializer "sprockets.spine_paginator", after: "append_asset_paths" do |app|
+    initializer "sprockets.spine_paginator", after: "append_asset_paths", group: :all do |app|
       next unless app.config.assets.enabled
-      app.config.assets.paths << File.expand_path("../../../dist", __FILE__)
+      path = File.expand_path("../../../dist", __FILE__)
+      app.config.assets.paths << path unless app.config.assets.paths.include?(path)
     end
 
   end
